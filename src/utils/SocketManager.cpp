e4e1322898c96d8da1ef2a96f40f64b6515f0526
LucT3 <luc.tart3@gmail.com>
2023-12-07 19:31:10 +0100

    socketManager structure

--- src/utils/SocketManager.cpp
+++ src/utils/SocketManager.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <cstring>
+#include <exception>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
 #include "SocketManager.h"
+
+
+int SocketManager::initSocket(string ip_address, int port) {
+    m_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    return 0;
+}
+
+
+SocketManager::SocketManager(string ip_address, int port, int max_requests) {
+    int res = initSocket(ip_address,port);
+
+}
+
+SocketManager::SocketManager() {
+
+}
+
+
+SocketManager::~SocketManager() {
+
+}
+
+int SocketManager::send() {
+}
+
+int SocketManager::receive() {
+
+}
+
+int SocketManager::accept() {
+
+}
--- src/utils/SocketManager.h
+++ src/utils/SocketManager.h
@@ -1,8 +1,18 @@
 #ifndef SECURE_CLOUD_STORAGE_SOCKETMANAGER_H
 #define SECURE_CLOUD_STORAGE_SOCKETMANAGER_H
 
+#include <string>
+
+using namespace std;
 
 class SocketManager {
+    int m_socket;
+
+public:
+    SocketManager(string ip_address, int port, int max_request);
+    ~SocketManager();
+
+    int initSocket(string ip_address, int port);
 
 };
 
