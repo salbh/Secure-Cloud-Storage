384ac99e1e3b1375ac58a665c8ca48a0757c63a7
Francesco Martoccia <92721828+FrankMartoccia@users.noreply.github.com>
2023-12-23 12:57:13 +0100

    Download M1 and Download M2 messages and the corresponding serialize and deserialize methods implemented.

--- src/messages/Download.cpp
+++ src/messages/Download.cpp
@@ -1,1 +1,103 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <openssl/rand.h>
 #include "Download.h"
+#include "CodesManager.h"
+
+using namespace std;
+
+Download::Download() = default;
+
+// Download M1
+Download::Download(const string& filename) {
+    m_message_code = static_cast<uint8_t>(Message::DOWNLOAD_REQUEST);
+
+    strncpy(m_filename, filename.c_str(), Config::FILE_NAME_LEN);
+}
+
+uint8_t* Download::serializeDownloadM1() {
+    // Allocate memory for the message buffer
+    uint8_t* message_buffer = new (nothrow) uint8_t[Config::MESSAGE_CODE_PACKET_SIZE];
+    // Check if memory allocation was successful
+    if (!message_buffer) {
+        cerr << "Download - Error during the serialization: Failed to allocate memory!" << endl;
+        return nullptr;
+    }
+    size_t current_buffer_position = 0;
+    // Copy the message code into the buffer
+    memcpy(message_buffer, &m_message_code, sizeof(uint8_t));
+    current_buffer_position += sizeof(uint8_t);
+    // Copy the filename into the buffer
+    memcpy(message_buffer + current_buffer_position, &m_filename, Config::FILE_NAME_LEN * sizeof(char));
+    current_buffer_position += Config::FILE_NAME_LEN * sizeof(char);
+    // Generate random bytes to fill the remaining space in the buffer
+    if (RAND_bytes(message_buffer + current_buffer_position,
+                   Config::MESSAGE_CODE_PACKET_SIZE - current_buffer_position) != 1) {
+        cerr << "Download - Error during serialization: RAND_bytes failed!" << endl;
+        delete[] message_buffer; // Release memory in case of failure
+        return nullptr;
+    }
+    // Return the serialized message buffer
+    return message_buffer;
+}
+
+Download Download::deserializeDownloadM1(uint8_t* message_buffer) {
+    // Create a Download object to store the deserialized message
+    Download downloadMessage;
+
+    size_t current_buffer_position = 0;
+
+    // Copy the message code from the buffer
+    memcpy(&downloadMessage.m_message_code, message_buffer, sizeof(uint8_t));
+    current_buffer_position += sizeof(uint8_t);
+    // Copy the filename from the buffer
+    memcpy(&downloadMessage.m_filename, message_buffer + current_buffer_position,
+           Config::FILE_NAME_LEN * sizeof(char));
+    // Return the deserialized Download message
+    return downloadMessage;
+}
+
+// Download M2
+Download::Download(const size_t& file_size) {
+    // Set the message code to indicate a Download Ack
+    m_message_code = static_cast<uint8_t>(Message::DOWNLOAD_ACK);
+
+    m_file_size = static_cast<uint32_t>(file_size);
+}
+
+uint8_t* Download::serializeDownloadM2() {
+    // Allocate memory for the message buffer
+    uint8_t*  message_buffer = new (nothrow) uint8_t[sizeof(uint8_t) + sizeof(uint32_t)];
+    // Check if memory allocation was successful
+    if (!message_buffer) {
+        cerr << "Download - Error during the serialization: Failed to allocate memory!" << endl;
+        return nullptr;
+    }
+
+    size_t current_buffer_position = 0;
+    // Copy the message code into the buffer
+    memcpy(message_buffer, &m_message_code, sizeof(uint8_t));
+    current_buffer_position += sizeof(uint8_t);
+    // Copy the size of the file to be downloaded into the buffer
+    memcpy(message_buffer + current_buffer_position, &m_file_size, sizeof(uint32_t));
+    // Return the serialized message buffer
+    return message_buffer;
+}
+
+Download Download::deserializeDownloadM2(uint8_t* message_buffer, const size_t& file_size) {
+    // Create a DownloadM2 object to store the deserialized message
+    Download downloadM2(file_size);
+
+    size_t current_buffer_position = 0;
+
+    // Copy the message code from the buffer
+    memcpy(&downloadM2.m_message_code, message_buffer, sizeof(uint8_t));
+    current_buffer_position += sizeof(uint8_t);
+    // Copy the size of the file to be downloaded from the buffer
+    memcpy(&downloadM2.m_file_size, message_buffer + current_buffer_position, sizeof(uint32_t));
+    // Return the deserialized Rename message
+    return downloadM2;
+}
+
+// Downlaod M3+i
--- src/messages/Download.h
+++ src/messages/Download.h
@@ -1,13 +1,31 @@
 #ifndef SECURE_CLOUD_STORAGE_DOWNLOAD_H
 #define SECURE_CLOUD_STORAGE_DOWNLOAD_H
 
+#include "Config.h"
+
+using namespace std;
 
 class Download {
-    //FRANK
-    //M1:(DOWNLOAD REQUEST, FILENAME)
-    //M2:(DOWNLOAD ACK, FILESIZE)
-    //M3+i:(DOWNLOAD RESPONSE,FILE CHUNK)
 
+private:
+
+    int m_message_code;
+    char m_filename[Config::FILE_NAME_LEN];
+    uint32_t m_file_size;
+
+public:
+    Download();
+
+    // M1:(DOWNLOAD REQUEST, FILENAME)
+    Download(const string &filename);
+    uint8_t *serializeDownloadM1();
+    Download deserializeDownloadM1(uint8_t *message_buffer);
+
+    //M2:(DOWNLOAD ACK, FILESIZE)
+    Download(const size_t &file_size);
+    uint8_t *serializeDownloadM2();
+    Download deserializeDownloadM2(uint8_t *message_buffer, const size_t &file_size);
+    //M3+i:(DOWNLOAD RESPONSE,FILE CHUNK)
 };
 
 
